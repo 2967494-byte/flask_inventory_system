f51e92b7efab1da5302df254f90a56d94589bbf1
SirRenat <r.sirazev@gmail.com>
2025-12-19 23:54:03 +0300

    Update WSGI application configuration.

--- wsgi.py
+++ wsgi.py
@@ -18,17 +18,17 @@ from app.models import User, Category, Product
 app = create_app()
 
 # ПРИНУДИТЕЛЬНОЕ СОЗДАНИЕ ТАБЛИЦ ПРИ ЗАПУСКЕ
-with app.app_context():
-    try:        
-        # Проверяем что таблицы существуют
-        from sqlalchemy import inspect
-        inspector = inspect(db.engine)
-        tables = inspector.get_table_names()
-        print(f"Таблицы в БД: {tables}")
+# with app.app_context():
+#    try:        
+#        # Проверяем что таблицы существуют
+#        from sqlalchemy import inspect
+#        inspector = inspect(db.engine)
+#        tables = inspector.get_table_names()
+#        print(f"Таблицы в БД: {tables}")
         
-    except Exception as e:
-        print(f"❌ Ошибка создания таблиц: {e}")
-        print(f"🔍 DATABASE_URL: {app.config['SQLALCHEMY_DATABASE_URI']}")
+#    except Exception as e:
+#        print(f"❌ Ошибка создания таблиц: {e}")
+#        print(f"🔍 DATABASE_URL: {app.config['SQLALCHEMY_DATABASE_URI']}")
 
 if __name__ == '__main__':
     app.run()
